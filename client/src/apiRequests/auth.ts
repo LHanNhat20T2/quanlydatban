e08145730acbf30ff3e80164a373e3f908bb93dd
HanNhat02 <hannhat2468@gmail.com>
2024-12-28 19:10:09 +0700

    code logic login

--- client/src/apiRequests/auth.ts
+++ client/src/apiRequests/auth.ts
@@ -4,7 +4,7 @@ import { LoginBodyType, LoginResType } from "@/schemaValidations/auth.schema";
 const authApiRequest = {
     sLogin: (body: LoginBodyType) =>
         http.post<LoginResType>("/auth/login", body),
-    Login: (body: LoginBodyType) =>
+    login: (body: LoginBodyType) =>
         http.post<LoginResType>("/api/auth/login", body, {
             baseUrl: "",
         }),
